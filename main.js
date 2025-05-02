2526e6daf5ce1487e042e1bc03dbe707588290a1
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-02 11:32:39 -0700

    Update main.js to add debug because something sdoes=voane work

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function goToCompounds() {
+  alert("hello!");
   try{
-    window.location.href = (window.location.href+"/compounds/list.html");
+    window.location.href = window.location.href+"/compounds/list.html";
   } catch (exception) {
     alert("An error: ", exception.message, " occured! Please relay this to the developer.");
   }
