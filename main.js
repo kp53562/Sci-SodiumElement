1d771baad296544a58911fd1af231d151a3cb509
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-02 11:30:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function goToCompounds() {
   try{
-    var ref = window.location.href.toString();
-    window.location.href = (ref.split((ref.length) -1)==/) ? (window.location.href+"compounds/list.html") : (window.location.href+"/compounds/list.html");
+    window.location.href = (window.location.href+"/compounds/list.html");
   } catch (exception) {
     alert("An error: ", exception.message, " occured! Please relay this to the developer.");
   }
