4a6d8881916724ed69803f77737bc6a65cb2114a
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-05 18:48:59 -0700

    Update interactive-demo.js

--- properties/interactive-demo.js
+++ properties/interactive-demo.js
@@ -1,6 +1,8 @@
 function startInteraction(){
   document.getElementById("demo-image").setAttribute("src","../assets/sodium-cut-2.png");
   document.getElementById("demo-two").setAttribute("style","visibility: visible;");
+  document.getElementById("action-button").innerHTML = "Reload the demo";
+  document.getElementById("action-button").setAttribute("onclick","refreshInteractiveExperience()");
 }
 function refreshInteractiveExperience(){
   window.location.reload();
