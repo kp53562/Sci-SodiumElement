b40d80ab0c7b333085910f5801150fcc704575b8
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-05 19:01:20 -0700

    Update interactive-demo.js

--- properties/interactive-demo.js
+++ properties/interactive-demo.js
@@ -3,6 +3,7 @@ function startInteraction(){
   document.getElementById("demo-two").setAttribute("style","visibility: visible;");
   document.getElementById("action-button").innerHTML = "Reload the demo";
   document.getElementById("action-button").setAttribute("onclick","refreshInteractiveExperience()");
+  document.getElementById("show-on-interaction-end").setAttribute("style","visibility: visible;");
 }
 function refreshInteractiveExperience(){
   window.location.reload();
