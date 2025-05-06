a138c510312ffe713431820c5327fdc64b9e2c83
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-05 18:46:48 -0700

    Create interactive-demo.js and add functionality

---
+++ properties/interactive-demo.js
@@ -0,0 +1,7 @@
+function startInteraction(){
+  document.getElementById("demo-image").setAttribute("src","../assets/sodium-cut-2.png");
+  document.getElementById("demo-two").setAttribute("style","visibility: visible;");
+}
+function refreshInteractiveExperience(){
+  window.location.reload();
+}
