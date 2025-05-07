72ab90559d77fa93f1f3869fb2713aef32af9579
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-06 18:47:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+var funFactsCount=1; // not advanced enough. I need ideas!
+function elemById(identification){
+  return document.getElementById(identification);
+}
 function properties() {
   window.location.href="https://kp53562.github.io/Sci-SodiumElement/properties/";
+}
+function revealFunFacts(){
+  if (funFactsCount<5){
+    elemById("r"+funFactsCount.toString()).setAttribute("style","visibility: visible;");
+    funFactsCount++;
+  }else{
+    elemById("funFactsButton").setAttribute("style","visibility: hidden;");
+  }
 }
