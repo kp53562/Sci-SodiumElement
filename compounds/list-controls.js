e4cdc2990696f1d508f56a007e3b5e063caa323a
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-07 21:26:03 -0700

    Update list-controls.js

--- compounds/list-controls.js
+++ compounds/list-controls.js
@@ -6,14 +6,14 @@ function hideOrShowList(listID){
   var listE = elemFromID(stringID);
   var imgE = elemFromID(stringID + "b");
   var showList = false;
-  if (imgE.getAttribute("src")=="..assets/expand-list_button.png"){showList = true;}
+  if (imgE.getAttribute("src")=="../assets/expand-list_button.png"){showList = true;}
   if (showList == false){
     listE.setAttribute("style","position: absolute; visibility: hidden;");
-    imgE.setAttribute("src","..assets/hide-list_button.png");
+    imgE.setAttribute("src","../assets/hide-list_button.png");
     imgE.setAttribute("alt","Collapse");
   }else{
     listE.setAttribute("style","position: static; visibility: visible;");
-    imgE.setAttribute("src","..assets/show-list_button.png");
+    imgE.setAttribute("src","../assets/show-list_button.png");
     imgE.setAttribute("alt","Expand");
   }
 }
