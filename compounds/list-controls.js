25546f7973a0ef82340407d65c5d9c969257b067
kp53562 <157652589+kp53562@users.noreply.github.com>
2025-05-07 21:14:27 -0700

    Update list-controls.js

--- compounds/list-controls.js
+++ compounds/list-controls.js
@@ -1,1 +1,17 @@
-
+function elemFromID(id){
+  return document.getElementById(id);
+}
+function hideOrShowList(listID){
+  var stringID = String(listID);
+  var listE = elemFromID(stringID);
+  var imgE = elemFromID(stringID + "b");
+  var showList = false;
+  if (imgE.getAttribute("src")=="..assets/expand-list-button.png"){showList = true;}
+  if (showList == false){
+    listE.setAttribute("style","position: absolute; visibility: hidden;");
+    imgE.setAttribute("src","..assets/hide-list-button.png");
+  }else{
+    listE.setAttribute("style","position: static; visibility: visible;");
+    imgE.setAttribute("src","..assets/show-list-button.png");
+  }
+}
